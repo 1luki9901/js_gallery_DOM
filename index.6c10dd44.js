26f18dec601488c3edbb5224378e927629f9f00f
Łukasz <1luki9901@gmail.com>
2024-10-30 23:29:26 +0100

    make build

---
+++ index.6c10dd44.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".gallery"),t=document.querySelector(".main-image img");e.addEventListener("click",function(e){var n=event.target;"A"===n.tagName&&(n=n.querySelector("img")),n&&"IMG"===n.tagName&&(t.src=n.src)})});
+//# sourceMappingURL=index.6c10dd44.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+    const gallery = document.querySelector(".gallery");
+    const mainImage = document.querySelector(".main-image img");
+    gallery.addEventListener("click", (e)=>{
+        let target = event.target;
+        if (target.tagName === "A") target = target.querySelector("img");
+        if (target && target.tagName === "IMG") mainImage.src = target.src;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
